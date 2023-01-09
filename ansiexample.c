cea1b271a8ea1d0a0b965df425ca76dcb7a5503b
ltoal <47865984+ltoal@users.noreply.github.com>
2023-01-09 09:59:10 -0800

    Update ansiexample.c
    background, clear

--- ansiexample.c
+++ ansiexample.c
@@ -2,10 +2,13 @@
 
 #define PURPLE "\033[38;5;177m"
 #define PINK "\033[38;5;219m"
+#define BACKGROUND "\033[48;5;105m"
+#define CLEAR "\033[0m"
 
 int main (int argc, char const *argv[]) {
 
-  printf(PURPLE "This text is COLOURED!\n");
-  printf(PINK "So is this!\n");
+  printf(PURPLE "This text is COLOURED!\n" CLEAR);
+  printf(PINK "So is this!\n" CLEAR);
+  printf(BACKGROUND "This has a background!\n" CLEAR);
   return 0;
 }
